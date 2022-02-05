392c305340dfa14ec7dd9f50c0e30ac15ed51195
Andrew Kelly <andrew@akelly.com>
2022-02-05 01:54:10 -0800

    Moved printing around a bit

--- source/Util/src/main_programs/main_condition_data.cpp
+++ source/Util/src/main_programs/main_condition_data.cpp
@@ -1,15 +1,18 @@
 
 // "g++ --std=c++1z -pthread program.cpp -lpthread -lstdc++fs -L /path/where/libUtil.so/resides/"
 
-#include "condition_data.hpp"
-#include "Utility.hpp"
 #include <thread>
 #include <vector>
 #include <algorithm>
 #include <string>
 #include <sstream>
+#include <ostream>
 #include <iostream>
 #include <chrono>
+#include <stdlib.h>
+#include "condition_data.hpp"
+#include "Utility.hpp"
+#include "commandline.hpp"
 
 /////////////////////////////////////////////////////////////////////////////////
 // MIT License
@@ -35,11 +38,70 @@
 // SOFTWARE.
 /////////////////////////////////////////////////////////////////////////////////
 
-int main(int, char**)
+// To run this program use:
+//
+// 		main_condition_data [num_threads]
+//
+// If num_threads is specified, it has to be numerical and greater than 0. If not, it
+// defaults to 20.
+//
+// For example:
+// $
+// $ cd /path/to/build/localrun
+// $
+// $ LD_LIBRARY_PATH=".:" ./main_condition_data 60
+//
+// By default, the output is sorted chronologically (when each thread finishes its work).
+// To sort the output by the thread number, pipe the output:                              | sort -k3 -n
+// To sort the output by the milliseconds' delay, pipe the output:                        | sort -k7 -n
+// To sort the output by the data obtained from the condition variable, pipe the output:  | sort -k10 -n
+//
+
+void Usage(std::ostream& strm, std::string command)
+{
+    strm << "Usage:    " << command << " [ num_threads ]\n\n" <<
+    		     "If num_threads is specified, it has to be numerical and greater than 0. ]\n" <<
+				 "If it is not specified, it defaults to 20.\n" << std::endl;
+}
+
+// Returns the number of threads requested, or 0 if no parameters.
+// Returns -1 if Usage() (help) was requested.
+int parse(int argc, const char *argv[])
+{
+    // If no parameters were supplied.
+	if (argc == 1)
+	{
+		return 0;
+	}
+	else if (std::string(argv[1]) == "--help" ||
+  	    	 std::string(argv[1]) == "-h" ||
+  	    	 std::string(argv[1]) == "help")
+    {
+        Usage(std::cerr, argv[0]);
+        return -1;
+    }
+	return strtol(argv[1], NULL, 10);
+}
+
+int main(int argc, const char *argv[])
 {
 	using namespace Util;
 
-    int numthreads = 15;
+    int numthreads = parse(argc, argv);
+    // DEBUG   std::cerr << "Parse returned: " << numthreads << std::endl;
+
+    if (numthreads == -1)
+    {
+    	Usage(std::cerr, argv[0]);
+    	return 1;
+    }
+    else if(numthreads == 0)
+    {
+    	numthreads = 20;
+    }
+
+    std::cerr << "Number of threads chosen: " << numthreads << std::endl;
+
     std::vector<int> sleeptimes;
     condition_data<int> condvar(0); // loads 0 as the initial data
 
@@ -51,41 +113,46 @@ int main(int, char**)
     // vector container stores threads
     std::vector<std::thread> workers;
 
+    // First create all the threads, enter them, and then wait
+    // for the go signal from the main program.
     for (int i = 1; i <= numthreads; i++) {
-        workers.push_back( std::thread([i,sleeptimes,&condvar]()
+    			    workers.push_back( std::thread([i,sleeptimes,&condvar]()
 					{
 						int slp = sleeptimes[i-1];
 						std::this_thread::sleep_for(std::chrono::milliseconds(slp));
 
+						// After sleeping for various times, all threads wait for the main()
+						// function to see if they should start. The condition variable allowing
+						// them to continue is set by the main() function after all threads have
+						// been initialized.
 						condvar.wait_for_ready();
 
 						// It's ready - get the data and reset it to a new value
 						int oldvalue = condvar.get_data();
 
+						//using this so that each thread output comes out on a single line
+						std::ostringstream lostr;
+						lostr << "thread function " << i << "  --  sleeping for " << slp << " -- got " << oldvalue << "\n";
+						std::cout << lostr.str() << std::flush;
+
 						// And set it free
 						condvar.send_ready(i);
-
-						//using this so that all thread output comes out on one line
-						std::ostringstream lostr;
-						lostr << "thread function " << i << "  --  sleeping for " << slp << " -- got " << oldvalue;
-
-						std::cout << lostr.str() << std::endl;
 					}));
     }
-    std::cout << "main thread\n";
+    std::cerr << "main thread: Sending ready message to all threads\n";
 
-    // Set the condition variable loose....
+    // All threads have been initialized.  Set the condition variable loose....
     condvar.send_ready(0);
 
     // wait for the last condition to be met...
     std::this_thread::sleep_for(std::chrono::milliseconds(400));
     condvar.wait_for_ready();
-    std::cout << "Last condition data: " << condvar.get_data() << std::endl;
+    std::cerr << "Last condition data: " << condvar.get_data() << std::endl;
 
     std::for_each(workers.begin(), workers.end(), [](std::thread &t)
-            {
-                t.join();
-            });
+    {
+    	if (t.joinable()) t.join();
+    });
 
     return 0;
 }
