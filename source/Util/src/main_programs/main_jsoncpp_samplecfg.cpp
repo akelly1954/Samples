0a25788b24e87b8b2d24f0ce084118276345ca8a
Andrew Kelly <andrew@akelly.com>
2022-09-22 14:04:13 -0700

    Added main_jsoncpp_samplecfg.cpp and its data main_jsoncpp_samplecfg.json to the Samples Util project.  Next we'll do Video.

---
+++ source/Util/src/main_programs/main_jsoncpp_samplecfg.cpp
@@ -0,0 +1,40 @@
+
+// main_jsoncpp_samplecfg.cpp
+
+#include <iostream>
+// #include <json/value.h>
+// #include <jsoncpp/json/json.h>
+#include <json/json.h>
+#include <fstream>
+
+void 
+displayCfg(const Json::Value &cfg_root);
+
+int
+main()
+{
+    Json::Reader reader;
+    Json::Value cfg_root;
+    std::ifstream cfgfile("main_jsoncpp_samplecfg.json");
+    cfgfile >> cfg_root;
+
+    std::cout << "______ cfg_root : start ______" << std::endl;
+    std::cout << cfg_root << std::endl;
+    std::cout << "______ cfg_root : end ________" << std::endl;
+
+    displayCfg(cfg_root);
+}       
+
+void 
+displayCfg(const Json::Value &cfg_root)
+{
+    std::string serverIP = cfg_root["Config"]["server-ip"].asString();
+    std::string serverPort = cfg_root["Config"]["server-port"].asString();
+    unsigned int bufferLen = cfg_root["Config"]["buffer-length"].asUInt();
+
+    std::cout << "______ Configuration ______" << std::endl;
+    std::cout << "server-ip     :" << serverIP << std::endl;
+    std::cout << "server-port   :" << serverPort << std::endl;
+    std::cout << "buffer-length :" << bufferLen<< std::endl;
+}
+
